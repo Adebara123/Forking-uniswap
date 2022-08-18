4eaa00cc229a410357f6ef19560f61eba39f7f78
Adebara Ayomide <adebaraayomide.com>
2022-08-18 06:54:16 +0100

    Add

--- scripts/forking.ts
+++ scripts/forking.ts
@@ -6,7 +6,7 @@ async function main() {
 
 // Contract of tokens we are swapping
 const USDT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
-const DAI_ADDRESS = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const DAI_ADDRESS = "0x6B175474E89094C44Da98b954EedeAC495271d0F";
     
 // ethereum address of the persn we are trying to impersonate
 const address = "0x308B675589C39afDF360d782F9DF2Ac4b42793dD";
@@ -24,8 +24,24 @@ console.log("Your usdt balance is", bal_usdt);
 console.log("Your dai balance is", bal_dai);
 
 // contract of uniswap helping us to swap
-const ROUTER_ADDRESS = 
-const UNISWAP = await ethers.getContractAt("IUniswap")
+const ROUTER_ADDRESS = "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D";
+const UNISWAP = await ethers.getContractAt("IUniswap", ROUTER_ADDRESS, impersonatedSigner);
+const amountOut = 50e6;
+const amountIn = 40e6;
+
+// uint amountOut,
+// uint amountInMax,
+// address[] calldata path,
+// address to,
+// uint deadline
+
+const swapToken = await UNISWAP.swapTokensForExactTokens(amountOut, amountIn, [USDT_ADDRESS, DAI_ADDRESS], address, 1660801829);
+
+
+// checking our balance after we swapped 
+console.log("Your usdt balance after swap is", bal_usdt);
+console.log("Your dai balance after swap is", bal_dai);
+
 
 
 
